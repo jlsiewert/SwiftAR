76e3b3b83bce180181890a20901e9fa03bec6636
Jan Luca Siewert <mail@jl-siewert.de>
2021-04-16 23:38:21 +0200

    Move to PlaygroundBook template

---
+++ SwiftARPlaygroundBook/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/PlaygroundPage.playgroundpage/main.swift
@@ -0,0 +1,11 @@
+//#-hidden-code
+//
+//  See LICENSE folder for this template’s licensing information.
+//
+//  Abstract:
+//  The Swift file containing the source code edited by the user of this playground book.
+//
+//#-end-hidden-code
+import SwiftAR
+
+
