b29bfae63d2eef043d863ba4be7fa22a17328986
Jan Luca Siewert <mail@jl-siewert.de>
2021-04-16 23:48:24 +0200

    Remove LiveView

--- SwiftARPlaygroundBook/LiveViewTestApp/AppDelegate.swift
+++ SwiftARPlaygroundBook/LiveViewTestApp/AppDelegate.swift
@@ -11,7 +11,11 @@ import LiveViewHost
 import SwiftAR
 
 struct LiveViewExperience: Experience {
-    
+    var body: some Anchor {
+        Surface {
+            Cube()
+        }
+    }
 }
 
 @UIApplicationMain
@@ -22,7 +26,7 @@ class AppDelegate: LiveViewHost.AppDelegate {
         // The view or view controller returned from this method will be automatically be shown on screen,
         // as if it were a live view in Swift Playgrounds. You can control how the live view is shown by
         // changing the implementation of the `liveViewConfiguration` property below.
-        return BookCore.instantiateLiveView()
+        return SCNRenderedViewController(experience: LiveViewExperience())
     }
 
     override var liveViewConfiguration: LiveViewConfiguration {
