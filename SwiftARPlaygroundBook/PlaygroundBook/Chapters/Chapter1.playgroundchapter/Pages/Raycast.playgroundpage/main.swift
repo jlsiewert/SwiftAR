3f95a34500116c69a5c8693cb5d88ffd21cd0f52
Jan Luca Siewert <mail@jl-siewert.de>
2021-04-18 11:34:08 +0200

    Description for Interactivity page

--- SwiftARPlaygroundBook/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/Raycast.playgroundpage/main.swift
+++ SwiftARPlaygroundBook/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/Raycast.playgroundpage/main.swift
@@ -1,11 +1,36 @@
 /*:
  # Interactivity
  
+ `SwiftAR` allows you to build interactive
+ experiences using the `@State`, `@Binding`, `@StateObject`
+ `@ObservedObject` and `@EnvironmentPbject` modifiers.
+ 
+ In this example, we use the `World` anchor type
+ that just starts a world tracking.
+ Objects can then be placed using the `transform`,
+ `translate`, `rotate` and `scale` modifiers.
+ 
+ For interactivity, the `onTap` modifier on a `Model`
+ triggers when the model is tapped on.
+ 
+ Similarly, the `onTap` modifier on the `World` anchor
+ performs a raycast against the world.
+ 
+ In this example, green spheres are placed whenever you
+ tap on the screen.
+ Tapping on a sphere (`RaycastResultVisualization`)
+ toggles its color between `.green` and `.gray`.
+ 
  */
 
 import Foundation
 import SwiftAR
 import simd
+
+/*:
+ `RaycastResult` wraps a `transform` into a
+ `Hashable` and `Identifiable` struct
+ */
 
 struct RaycastResult: Hashable, Identifiable {
     let id = UUID()
@@ -14,6 +39,14 @@ struct RaycastResult: Hashable, Identifiable {
         hasher.combine(id)
     }
 }
+
+/*:
+ The raycast results gets visualized by a
+ small sphere whose color can be toggled
+ between `.green` and `.gray`.
+ The `animation` modifiers animates these
+ changes in the material.
+ */
 
 struct RaycastResultVisualization: Model {
     let transform: simd_float4x4
@@ -29,6 +62,11 @@ struct RaycastResultVisualization: Model {
             }
     }
 }
+
+/*:
+ The experience stores an array of `RaycastResults`
+ and displays a sphere whenever the user taps on the screen.
+ */
 
 struct PlaygroundExperience: Experience {
     @State var raycasts: [RaycastResult] = []
